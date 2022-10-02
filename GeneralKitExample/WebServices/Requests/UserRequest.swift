a4523dd34836c5eb5886e50d44b8a48ddd886131
Salah <Salahalimohamed1995@gmail.com>
2022-10-02 21:22:50 +0300

    no message

---
+++ GeneralKitExample/WebServices/Requests/UserRequest.swift
@@ -0,0 +1,60 @@
+//
+//  UserRequest.swift
+//  GeneralKitExample
+//
+//  Created by Salah on 10/1/22.
+//
+
+import Foundation
+import GeneralKit
+import Alamofire
+import ObjectMapper
+import AlamofireObjectMapper
+
+class UserRequest:AppBaseRequest{
+    override var baseUrl:String{
+    return  "http://nfcard.online/Salah/"
+    }
+    public enum Route{
+        case users
+        case login
+    }
+    private var route:Route
+    init(_ route:Route) {
+        self.route = route
+    }
+    override var path:String?{
+        switch self.route{
+        case .users:
+            return "usersList\(self.page ?? "1").json";
+        case .login:
+            return "login"
+        }
+    }  // for request path
+    override var header : HTTPHeaders?{
+        return nil
+    }
+    override var parameters:Dictionary<String,String>{
+        let parameters =  super.parameters
+        switch self.route{
+        case .users:
+            break;
+        case .login:
+            break;
+        }
+        return parameters;
+    } // request paramter
+    override var type:HTTPMethod!{
+        switch self.route{
+        case .login,.users:
+            return .get
+        }
+    } // for post type : .post,.get,.delete
+    override var multiPartObjects : [ValidationObject.MultiPartObject]{
+        let items = [ValidationObject.MultiPartObject]();
+        switch self.route{
+        case .users,.login:
+            return items
+        }
+    }
+}
